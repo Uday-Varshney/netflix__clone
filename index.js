88e43d8e96cbfbc0bb3a3790ef175f27e0a3e68a
Uday Varshney <uv.uday.varshney@gmail.com>
2023-03-22 13:54:18 +0530

    ..

---
+++ index.js
@@ -0,0 +1,142 @@
+// Your web app's Firebase configuration//
+var firebaseConfig = {
+  apiKey: "AIzaSyDLEe7U72xokoQkDXNggq9woGmCRcLF4QQ",
+  authDomain: "netphlix-31d3b.firebaseapp.com",
+  projectId: "netphlix-31d3b",
+  storageBucket: "netphlix-31d3b.appspot.com",
+  messagingSenderId: "414604212037",
+  appId: "1:414604212037:web:de2eef4895297c2195302c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  full_name = document.getElementById("fullname").value;
+  phonenum = document.getElementById("phnum").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+    // Don't continue running the code
+  }
+  if (validate_field(full_name) == false) {
+    alert("One or More Extra Fields is Outta Line!!");
+    return;
+  }
+
+  // Move on with Auth
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        email: email,
+        full_name: full_name,
+        phone: phonenum,
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      // create a colletion name users in the firebase.
+      database_ref.child("users/" + user.uid).set(user_data);
+
+      // DOne
+      alert("User Created!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+    // Don't continue running the code
+  }
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).update(user_data);
+
+      // DOne
+      alert("User Logged In!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false;
+  }
+
+  if (field.length <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
